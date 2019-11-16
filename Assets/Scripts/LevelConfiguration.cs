dcf95a872f313a2f3885c6f2b2d6ff856de42663
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2019-11-16 13:19:07 +0300

    Add level configuration script

---
+++ Assets/Scripts/LevelConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/LevelConfiguration")]
+public class LevelConfiguration : ScriptableObject
+{
+    public int RowCount;
+    public int ColumnCount;
+    public Vector2Int LevelEnter;
+    public Vector2Int LevelExit;
+    public GameObject EnemyPrefab;
+    public GameObject ResourcePrefab;
+    public List<Vector2Int> EnemiesPositions;
+    public List<Vector2Int> ResourcesPositions;
+}
